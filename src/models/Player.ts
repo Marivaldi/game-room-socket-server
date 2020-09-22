2ed6d3cd299db682aa1e6981ff3a3537b0957258
Marivaldi <55033038+Marivaldi@users.noreply.github.com>
2020-09-22 18:00:56 -0400

    Tech Debt: Removed some dead code

--- src/models/Player.ts
+++ src/models/Player.ts
@@ -5,7 +5,7 @@ import ISocketMessage from "./socket-messages/interfaces/ISocketMessage";
 export class Player {
     public lobbyId: string;
     public username: string;
-    
+
     constructor(public connectionId: string, private socket: WebSocket) {
         this.acknowledgeConnection();
     }
@@ -17,18 +17,6 @@ export class Player {
     send(socketMessage: ISocketMessage) {
         this.socket.send(socketMessage.serialize());
     }
-
-    // onMessage = (json: string) => {
-    //     const message: ISocketMessage = JSON.parse(json);
-    //     if (!message || !message.type)  return;
-
-
-    //         case "SEND_LOBBY_CHAT":
-    //             const sender = get_lobby_username(message.lobby_id, connection_id);
-    //             send_message_to_lobby(message.lobby_id, message.message, sender, connection_id)
-    //             break;
-    //     }
-    // }
 
     private acknowledgeConnection() {
         this.send(new ConnectedMessage(this.connectionId));
